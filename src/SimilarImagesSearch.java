2b9f149de122e96411abb9a026e6b73ddd7e508b
O.Mrost <olgamrost@gmail.com>
2016-06-17 18:04:55 +0200

    corrected SearchSimilarImages method
    changed two lines

--- src/SimilarImagesSearch.java
+++ src/SimilarImagesSearch.java
@@ -18,24 +18,22 @@ public class SimilarImagesSearch {
 
 			HttpClient client = HttpClientBuilder.create().build();
 			HttpGet request = new HttpGet(url);
-			HttpResponse response = client.execute(request);
-
 			request.setHeader("Ocp-Apim-Subscription-Key", "08b669df660c40ed8135ac6403d859be");
 
+			HttpResponse response = client.execute(request);
+			
 			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 			StringBuffer body = new StringBuffer();
 			String line;
 			while ((line = reader.readLine()) != null) {
 				body.append(line);
 			}
-			System.out.println("searching");
 			System.out.println(body);
-			System.out.println("end of body");
-			System.out.println(body.toString());
+			
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block e.printStackTrace();
+			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block e.printStackTrace();
+			e.printStackTrace();
 		}
 
 	}
