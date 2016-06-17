28b0ea839723c2ca1373f97ceb3988f845b350a9
O.Mrost <olgamrost@gmail.com>
2016-06-17 17:59:40 +0200

    correct method to search for similar images
    class SimilarImagesSearch, now as main method

--- src/SimilarImagesSearch.java
+++ src/SimilarImagesSearch.java
@@ -14,13 +14,14 @@ public class SimilarImagesSearch {
 
 		try {
 
-			String url = "https://bingapis.azure-api.net/api/v5/images/search?q=sailing+dinghies&mkt=en-us";
+			String url = "https://bingapis.azure-api.net/api/v5/images/search?q=cat&mkt=en-us";
 
 			HttpClient client = HttpClientBuilder.create().build();
 			HttpGet request = new HttpGet(url);
-			HttpResponse response = client.execute(request);
-
+			
 			request.setHeader("Ocp-Apim-Subscription-Key", "08b669df660c40ed8135ac6403d859be");
+
+			HttpResponse response = client.execute(request);
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 			StringBuffer body = new StringBuffer();
@@ -28,14 +29,12 @@ public class SimilarImagesSearch {
 			while ((line = reader.readLine()) != null) {
 				body.append(line);
 			}
-			System.out.println("searching");
+			
 			System.out.println(body);
-			System.out.println("end of body");
-			System.out.println(body.toString());
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block e.printStackTrace();
+			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block e.printStackTrace();
+			e.printStackTrace();
 		}
 
 	}
