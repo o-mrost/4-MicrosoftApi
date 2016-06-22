2091b2423dc1530004f8604a74fc249bab86aa34
o-mrost <olgamrost@gmail.com>
2016-06-22 16:01:36 +0200

    added bing api (#18)

---
+++ src/HttpBing.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class HttpBing {
+
+	public String GetUrlContentAsString(String searchToken) {
+
+		StringBuffer body = null;
+
+		try {
+
+			String url = "https://bingapis.azure-api.net/api/v5/images/search?q=cat&count=1&mkt=en-us";
+
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpGet request = new HttpGet(url);
+			request.setHeader("Ocp-Apim-Subscription-Key", searchToken);
+
+			HttpResponse response = client.execute(request);
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			body = new StringBuffer();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				body.append(line);
+			}
+
+			System.out.println(body);
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return body.toString();
+
+	}
+}
